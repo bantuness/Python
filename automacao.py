421f60a36b760fa4bb30927dcdd7964123025c62
bantuness <86695349+bantuness@users.noreply.github.com>
2022-08-11 00:44:53 -0300

    Inclusões
    #Executar Webdriver
    #Entrar na página do Google
    #Clicar na barra de pesquisa e escrever algo "" <-- Ainda não finalizado.

--- automacao.py
+++ automacao.py
@@ -1,1 +1,7 @@
-print ('hello word')
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(executable_path=r"C:\Users\bruno\Documents\MeuProjetosPython\Python\chromedriver\chromedriver.exe") #Executar Webdriver
+driver.get("https://www.google.com.br/") #Entrar na página do Google
+element = driver.find_element(By.NAME, 'q').send_keys("abc") #Clicar na barra de pesquisa e escrever algo ""
