a2568fb069ca24f690ab83724ff9b6fcc68a9ef6
bantuness <86695349+bantuness@users.noreply.github.com>
2022-08-11 01:12:31 -0300

    .
    Teste

--- automacao.py
+++ automacao.py
@@ -4,4 +4,4 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path=r"C:\Users\bruno\Documents\MeuProjetosPython\Python\chromedriver\chromedriver.exe") #Executar Webdriver
 driver.get("https://www.google.com.br/") #Entrar na página do Google
-element = driver.find_element(By.NAME, 'q').send_keys("abc") #Clicar na barra de pesquisa e escrever algo ""
+driver.find_element(By.NAME, 'q').send_keys("abc") #Clicar na barra de pesquisa e escrever algo ""
